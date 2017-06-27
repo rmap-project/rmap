0fc8965900cdceacab8fb2df69f109857719d68f
Elliot Metsger <emetsger@jhu.edu>
2017-06-27 16:42:30 -0400

    Add classes to manage the persistence of the database and triplestore for integration tests. - Adds 'spring-util' module - Updates 'api' and 'webapp' module to depend on 'spring-util'. - The webapps need spring-util for integration tests.
    Unfortunately, spring-util is not able to be dropped into a shared classloader at the moment, due to some issues with Spring type conversion and casting object instances across classloaders.  Currently, then, spring-util is included as a runtime component of the webapps.  In the future alternate approaches could be taken (e.g. copying the spring-util jar into each webapp's class loader (WEB-INF/lib) before ITs start).

---
+++ spring-util/src/main/java/info/rmapproject/spring/jdbc/support/ConditionalDataSourceInitializer.java
@@ -0,0 +1,79 @@
+package info.rmapproject.spring.jdbc.support;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.stereotype.Component;
+
+import javax.sql.DataSource;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.function.Function;
+
+/**
+ * Only initializes the database if the {@code DataSourceInitializer} {@link DataSourceInitializer#enabled is enabled}
+ * and <em>any</em> {@link #getConditions() conditions} are {@code true}.
+ *
+ * @author Elliot Metsger (emetsger@jhu.edu)
+ */
+public class ConditionalDataSourceInitializer extends DataSourceInitializer {
+
+    private static Logger LOG = LoggerFactory.getLogger(ConditionalDataSourceInitializer.class);
+
+    private boolean enabled;
+
+    private DataSource dataSource;
+
+    private List<Function<DataSource, Boolean>> conditions = new ArrayList<>();
+
+    public List<Function<DataSource, Boolean>> getConditions() {
+        return conditions;
+    }
+
+    public void setConditions(List<Function<DataSource, Boolean>> conditions) {
+        if (conditions == null || conditions.size() == 0) {
+            throw new IllegalArgumentException("Conditions List must not be null or empty!");
+        }
+        this.conditions = conditions;
+    }
+
+    @Override
+    public void setEnabled(boolean enabled) {
+        super.setEnabled(enabled);
+        this.enabled = enabled;
+    }
+
+    @Override
+    public void setDataSource(DataSource dataSource) {
+        super.setDataSource(dataSource);
+        this.dataSource = dataSource;
+    }
+
+    @Override
+    public void afterPropertiesSet() {
+        if (this.enabled && anyConditionTrue(dataSource, conditions)) {
+            super.afterPropertiesSet();
+        }
+    }
+
+    private static boolean anyConditionTrue(DataSource dataSource, List<Function<DataSource, Boolean>> conditions) {
+        final AtomicBoolean anyConditionHoldsTrue = new AtomicBoolean(Boolean.FALSE);
+        final String msg = "Checking condition '%s': [%s]";
+        conditions.forEach(condition -> {
+            String conditionName = condition.getClass().getSimpleName();
+            if (condition.apply(dataSource)) {
+                LOG.debug(String.format(msg, conditionName, "true"));
+                anyConditionHoldsTrue.set(true);
+            } else {
+                LOG.debug(String.format(msg, conditionName, "false"));
+            }
+        });
+
+        if (anyConditionHoldsTrue.get()) {
+            LOG.debug("At least one condition held true, database will be initialized.");
+        }
+
+        return anyConditionHoldsTrue.get();
+    }
+}
