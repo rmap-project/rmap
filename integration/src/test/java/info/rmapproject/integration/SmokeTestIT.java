19bba1f7521853cbe268333ef6c17004bc7c7f3c
Elliot Metsger <emetsger@jhu.edu>
2017-06-28 09:30:34 -0400

    Integration tests and fixtures. - Includes developer documentation - Renames 'sesamehttp.*' properties to 'sesamehttp.repository.*' - Updates dependencies for the integration project - Starts a network server instance for Derby using beanshell (this instance is shared by the ITs and the RMap webapps) - Updates Cargo to start the OpenRDF Seseme HTTP server and Workbench webapps, along with the RMap webapps - Switch to using Tomcat instead of Jetty, due to an issue with request handling in Jetty for the OpenRDF webapps - Integration bean profiles are added to the shared classloader - Locations for the logback logging configuration and JUL logging configuration are specified using system properties - Spring beans used by the ITs are configured by the `FixtureConfig` class

--- integration/src/test/java/info/rmapproject/integration/SmokeTestIT.java
+++ integration/src/test/java/info/rmapproject/integration/SmokeTestIT.java
@@ -1,22 +1,44 @@
 package info.rmapproject.integration;
 
+import info.rmapproject.integration.fixtures.FixtureConfig;
+import info.rmapproject.spring.triplestore.support.TriplestoreManager;
+import okhttp3.MediaType;
 import okhttp3.OkHttpClient;
 import okhttp3.Request;
+import okhttp3.RequestBody;
 import okhttp3.Response;
-import org.junit.Before;
+import org.apache.commons.io.IOUtils;
 import org.junit.BeforeClass;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import java.net.MalformedURLException;
+import javax.sql.DataSource;
+import java.io.IOException;
 import java.net.URL;
+import java.sql.Connection;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.Base64;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
 /**
+ * Basic integration tests against the RMap API and HTML UI web applications.
+ *
  * @author Elliot Metsger (emetsger@jhu.edu)
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes = {FixtureConfig.class})
+@TestPropertySource({"classpath:/rmap.properties", "classpath:/integration-db.properties"})
 public class SmokeTestIT {
 
     private static String scheme = "http";
@@ -33,46 +55,114 @@ public class SmokeTestIT {
 
     private static URL appBaseUrl;
 
+    @Autowired
+    private TriplestoreManager tsManager;
+
+    @Autowired
     private OkHttpClient http;
 
-    @BeforeClass
-    public static void setUp() throws Exception {
-        assertNotNull("System property 'rmap.webapp.test.port' must be specified.", port);
+    @Autowired
+    private DataSource ds;
 
+    @BeforeClass
+    public static void setUpBaseUrls() throws Exception {
+        Logger.getLogger(OkHttpClient.class.getName()).setLevel(Level.FINE);
+        assertNotNull("System property 'rmap.webapp.test.port' must be specified.", port);
         assertTrue("System property 'rmap.webapp.test.port' must be an integer greater than 0",
                 Integer.parseInt(port) > 0);
-
         assertNotNull("System property 'rmap.webapp.context' must be specified.", webappCtxPath);
-
         assertNotNull("System property 'rmap.api.context' must be specified.", apiCtxPath);
-
-        setBaseUrls();
+        apiBaseUrl = new URL(scheme, host, Integer.parseInt(port), apiCtxPath);
+        appBaseUrl = new URL(scheme, host, Integer.parseInt(port), webappCtxPath);
     }
 
-    @Before
-    public void setUpOkHttp() throws Exception {
-        http = new OkHttpClient();
-    }
-
+    /**
+     * The RMap API webapp should return a 200, indicating successful startup.
+     *
+     * @throws IOException
+     */
     @Test
-    public void testApi200Ok() throws Exception {
+    public void testApi200Ok() throws IOException {
         URL url = new URL(apiBaseUrl, apiCtxPath + "/discos");
         Response res = http.newCall(new Request.Builder().get().url(url).build()).execute();
         assertEquals(url.toString() + " failed with: '" + res.code() + "', '" + res.message() + "'",
                 200, res.code());
     }
 
+    /**
+     * The RMap HTML webapp should return a 200, indicating successful startup.
+     *
+     * @throws IOException
+     */
     @Test
-    public void testWebapp200Ok() throws Exception {
+    public void testWebapp200Ok() throws IOException {
         Response res = http.newCall(new Request.Builder().get().url(appBaseUrl).build()).execute();
         assertEquals(appBaseUrl.toString() + " failed with: '" + res.code() + "', '" + res.message() + "'",
                 200, res.code());
     }
 
+    /**
+     * The DataSource for the database should be populated with one user.
+     *
+     * @throws SQLException
+     */
+    @Test
+    public void testDataSourceOk() throws SQLException {
+        assertNotNull(ds);
+        Connection c = ds.getConnection();
+        assertNotNull(c);
+        ResultSet rs = c.prepareCall("SELECT * from USERS").executeQuery();
+        assertTrue(rs.next());
+        assertFalse(rs.next());
+    }
 
-    private static void setBaseUrls() throws MalformedURLException {
-        apiBaseUrl = new URL(scheme, host, Integer.parseInt(port), apiCtxPath);
-        appBaseUrl = new URL(scheme, host, Integer.parseInt(port), webappCtxPath);
+    /**
+     * The user should be able to create a new disco.  Insure that a user that doesn't exist cannot create a disco.
+     * Insure that an unauthenticated request cannot create a disco.
+     *
+     * @throws IOException
+     */
+    @Test
+    public void testAuthenticatedRequest() throws IOException {
+        String accessKey = "uah2CKDaBsEw3cEQ";
+        String secret = "NSbdzctrP46ZvhTi";
+        URL url = new URL(apiBaseUrl, apiCtxPath + "/discos");
+        String sampleDisco = IOUtils.toString(this.getClass().getResourceAsStream("/discos/discoA.ttl"));
+
+        try (Response res =
+                     http.newCall(new Request.Builder()
+                             .post(RequestBody.create(MediaType.parse("text/turtle"), sampleDisco))
+                             .url(url).addHeader("Authorization", "Basic " + encodeAuthCreds(accessKey, secret))
+                             .build())
+                             .execute()) {
+            assertEquals(url.toString() + " failed with: '" + res.code() + "', '" + res.message() + "'",
+                    201, res.code());
+        }
+
+        try (Response res =
+                     http.newCall(new Request.Builder()
+                .post(RequestBody.create(MediaType.parse("text/turtle"), sampleDisco))
+                .url(url)
+                .addHeader("Authorization", "Basic " + encodeAuthCreds("foo", "bar"))
+                .build())
+                .execute()) {
+            assertEquals(url.toString() + " failed with: '" + res.code() + "', '" + res.message() + "'",
+                    401, res.code());
+        }
+
+        try (Response res =
+                     http.newCall(new Request.Builder()
+                .post(RequestBody.create(MediaType.parse("text/turtle"), sampleDisco))
+                .url(url)
+                .build())
+                .execute()) {
+            assertEquals(url.toString() + " failed with: '" + res.code() + "', '" + res.message() + "'",
+                    401, res.code());
+        }
+    }
+
+    private static String encodeAuthCreds(String accessKey, String secret) {
+        return Base64.getEncoder().encodeToString(String.valueOf(accessKey + ":" + secret).getBytes());
     }
 
 }
