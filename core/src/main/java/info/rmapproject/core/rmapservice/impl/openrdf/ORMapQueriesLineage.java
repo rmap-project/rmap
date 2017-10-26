6d86cc7858ee57e92dd9971a70b8676bcae82a5d
Aaron Birkland <apb@jhu.edu>
2017-10-26 16:27:47 -0400

    Add query for finding lineage of a DiSCO

---
+++ core/src/main/java/info/rmapproject/core/rmapservice/impl/openrdf/ORMapQueriesLineage.java
@@ -0,0 +1,86 @@
+/*
+ * Copyright 2017 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This software was produced as part of the RMap Project (http://rmap-project.info),
+ * The RMap Project was funded by the Alfred P. Sloan Foundation and is a
+ * collaboration between Data Conservancy, Portico, and IEEE.
+ */
+
+package info.rmapproject.core.rmapservice.impl.openrdf;
+
+import static info.rmapproject.core.utils.Terms.PROV_GENERATED_PATH;
+import static info.rmapproject.core.utils.Terms.RMAP_DELETEDOBJECT_PATH;
+import static info.rmapproject.core.utils.Terms.RMAP_EVENT_PATH;
+import static info.rmapproject.core.utils.Terms.RMAP_INACTIVATEDOBJECT_PATH;
+import static info.rmapproject.core.utils.Terms.RMAP_LINEAGE_PROGENITOR_PATH;
+import static info.rmapproject.core.utils.Terms.RMAP_UPDATEDOBJECT_PATH;
+
+import java.net.URI;
+
+import org.openrdf.query.TupleQuery;
+import org.openrdf.query.TupleQueryResult;
+import org.openrdf.repository.RepositoryConnection;
+
+import info.rmapproject.core.rmapservice.impl.openrdf.triplestore.SesameTriplestore;
+
+/**
+ * Lineage-related sparql queries and lookups
+ *
+ * @author apb@jhu.edu
+ */
+abstract class ORMapQueriesLineage {
+
+    static final String BINDING_LINEAGE = "lineage";
+
+    static final String BINDING_RESOURCE = "resource";
+
+    static final String QUERY_LINEAGE_SEARCH =
+            String.format("SELECT DISTINCT ?%s\n", BINDING_LINEAGE) +
+                    "WHERE {\nGRAPH ?g { \n" +
+                    String.format("?e a <%s> .\n", RMAP_EVENT_PATH) +
+                    String.format("?e <%s> ?%s .\n", RMAP_LINEAGE_PROGENITOR_PATH, BINDING_LINEAGE) +
+                    String.format("?e ?rel ?%s .\n", BINDING_RESOURCE) +
+                    String.format("FILTER (?rel IN (<%s>, <%s>, <%s>, <%s>) )",
+                            PROV_GENERATED_PATH,
+                            RMAP_UPDATEDOBJECT_PATH,
+                            RMAP_DELETEDOBJECT_PATH,
+                            RMAP_INACTIVATEDOBJECT_PATH) +
+                    "}}";
+
+    static URI findLineage(URI object, SesameTriplestore ts) {
+        try (RepositoryConnection c = ts.getConnection()) {
+
+            final TupleQuery q = c.prepareTupleQuery(QUERY_LINEAGE_SEARCH);
+
+            q.setBinding(BINDING_RESOURCE, c.getValueFactory().createIRI(object.toString()));
+
+            System.out.println(QUERY_LINEAGE_SEARCH);
+
+            try (TupleQueryResult result = q.evaluate()) {
+                if (result.hasNext()) {
+                    final URI found = URI.create(result.next().getBinding(BINDING_LINEAGE).getValue().toString());
+                    if (result.hasNext()) {
+                        throw new RuntimeException(String.format("Two lineages found for resource <>: <> and <>",
+                                object, found, result.next().getBinding(BINDING_LINEAGE).toString()));
+                    }
+                    return found;
+                }
+            }
+        }
+
+        return null;
+    }
+
+}
---
+++ core/src/test/java/info/rmapproject/core/rmapservice/impl/openrdf/ORMapQueriesLineageTest.java
@@ -0,0 +1,128 @@
+/*
+ * Copyright 2017 Johns Hopkins University
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * This software was produced as part of the RMap Project (http://rmap-project.info),
+ * The RMap Project was funded by the Alfred P. Sloan Foundation and is a
+ * collaboration between Data Conservancy, Portico, and IEEE.
+ */
+
+package info.rmapproject.core.rmapservice.impl.openrdf;
+
+import static info.rmapproject.core.model.event.RMapEventTargetType.DISCO;
+import static info.rmapproject.core.model.impl.openrdf.ORAdapter.uri2OpenRdfIri;
+import static info.rmapproject.core.rmapservice.impl.openrdf.ORMapQueriesLineage.findLineage;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+import java.net.URI;
+import java.util.Arrays;
+import java.util.Date;
+import java.util.UUID;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import info.rmapproject.core.CoreTestAbstract;
+import info.rmapproject.core.model.RMapIri;
+import info.rmapproject.core.model.impl.openrdf.ORMapEventCreation;
+import info.rmapproject.core.model.impl.openrdf.ORMapEventDerivation;
+import info.rmapproject.core.model.impl.openrdf.ORMapEventUpdate;
+import info.rmapproject.core.model.request.RequestEventDetails;
+import info.rmapproject.core.rmapservice.impl.openrdf.triplestore.SesameTriplestore;
+
+/**
+ * @author apb@jhu.edu
+ */
+public class ORMapQueriesLineageTest extends CoreTestAbstract {
+
+    @Autowired
+    private SesameTriplestore ts;
+
+    @Autowired
+    private ORMapEventMgr eventmgr;
+
+    URI discoURI;
+
+    URI lineageURI;
+
+    @Before
+    public void init() {
+        discoURI = randomURI();
+        lineageURI = randomURI();
+    }
+
+    @Test
+    public void viaCreateEventTest() {
+
+        final ORMapEventCreation event = new ORMapEventCreation(
+                uri2OpenRdfIri(randomURI()),
+                new RequestEventDetails(randomURI()), DISCO,
+                Arrays.asList(new RMapIri(discoURI)));
+        event.setEndTime(new Date());
+        event.setLineageProgenitor(new RMapIri(lineageURI));
+
+        eventmgr.createEvent(event, ts);
+
+        assertEquals(lineageURI, findLineage(discoURI, ts));
+    }
+
+    @Test
+    public void viaUpdateEventTest() {
+
+        final URI oldDiscoUri = randomURI();
+
+        final ORMapEventUpdate event = new ORMapEventUpdate(
+                uri2OpenRdfIri(randomURI()),
+                new RequestEventDetails(randomURI()), DISCO,
+                uri2OpenRdfIri(oldDiscoUri),
+                uri2OpenRdfIri(discoURI));
+
+        event.setEndTime(new Date());
+        event.setLineageProgenitor(new RMapIri(lineageURI));
+
+        eventmgr.createEvent(event, ts);
+
+        // Discovering the same lineage from both the new and old discos is expected
+        assertEquals(lineageURI, findLineage(oldDiscoUri, ts));
+        assertEquals(lineageURI, findLineage(discoURI, ts));
+    }
+
+    @Test
+    public void viaDerivationTest() {
+
+        final URI oldDiscoUri = randomURI();
+
+        final ORMapEventDerivation event = new ORMapEventDerivation(
+                uri2OpenRdfIri(randomURI()),
+                new RequestEventDetails(randomURI()), DISCO,
+                uri2OpenRdfIri(oldDiscoUri), uri2OpenRdfIri(discoURI));
+
+        event.setEndTime(new Date());
+        event.setLineageProgenitor(new RMapIri(lineageURI));
+
+        eventmgr.createEvent(event, ts);
+
+        // We shouldn't find the lineage of the thing that this event was derived from,
+        // we should only find the lineage of the newly created (derived) object
+        assertEquals(lineageURI, findLineage(discoURI, ts));
+        assertNull(findLineage(oldDiscoUri, ts));
+    }
+
+    private URI randomURI() {
+        return URI.create("urn:uuid:" + UUID.randomUUID().toString());
+    }
+
+}
