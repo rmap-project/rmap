d36b03a2d0ffb18649b50305196370b82f748046
Elliot Metsger <emetsger@jhu.edu>
2018-01-08 13:32:30 -0500

    Move IndexingConsumerIT from the indexing-solr module to the integration module.  Minor updates to the IT to squelch obnoxious output, and to update logging messages.  Add a dependency on the test jar produced by the indexing-solr module to the integration module.

---
+++ integration/src/test/java/info/rmapproject/indexing/kafka/BaseKafkaIT.java
@@ -0,0 +1,25 @@
+package info.rmapproject.indexing.kafka;
+
+import info.rmapproject.core.rdfhandler.RDFHandler;
+import org.junit.runner.RunWith;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+/**
+ * @author Elliot Metsger (emetsger@jhu.edu)
+ */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ActiveProfiles({"default", "integration-db", "inmemory-idservice", "integration-triplestore", "http-solr", "prod-kafka"})
+@ContextConfiguration({"classpath*:/rmap-indexing-solr.xml", "classpath*:/spring-rmapcore-context.xml", "classpath*:/rmap-kafka-shared-test.xml", "classpath*:/spring-rmapauth-context.xml"})
+public class BaseKafkaIT {
+
+    protected static final Logger LOG = LoggerFactory.getLogger(BaseKafkaIT.class);
+
+    @Autowired
+    protected RDFHandler rdfHandler;
+
+}
