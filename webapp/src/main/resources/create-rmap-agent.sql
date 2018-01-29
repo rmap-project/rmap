792025c09966b8444ad11a1621d5e543c2f1e500
Elliot Metsger <emetsger@jhu.edu>
2018-01-29 00:32:56 -0500

    Insure that each copy of "create-rmap-agent.sql" uses conditional insert logic.

--- webapp/src/main/resources/create-rmap-agent.sql
+++ webapp/src/main/resources/create-rmap-agent.sql
@@ -1,7 +1,20 @@
-INSERT into Users(name, email, isActive, rmapAgentUri, authKeyUri, doRMapAgentSync, createdDate, lastAccessedDate) values ('RMap Test User', 'testuser@example.com', TRUE, 'rmap:testagenturi', 'http://rmap-hub.org/authids/testauthid', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+INSERT into Users(name, email, isActive, rmapAgentUri, authKeyUri, doRMapAgentSync, createdDate, lastAccessedDate)
+SELECT
+  'RMap Test User', 'testuser@example.com', TRUE, 'rmap:testagenturi', 'http://rmap-hub.org/authids/testauthid', TRUE, CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+WHERE NOT EXISTS (
+  SELECT * FROM Users WHERE rmapAgentUri = 'rmap:testagenturi'
+);
 
 INSERT into UserIdentityProviders(identityProvider, providerAccountId, providerAccountPublicId, providerAccountDisplayName, providerAccountProfileUrl, userId, createdDate, LASTAUTHENTICATEDDATE)
-values ('http://exampleidprovider.org', '1234567890', 'rmaptestuser', 'RMap Test User', 'https://exampleidprovider.org/rmaptestuser', (select userId from Users where name='RMap Test User'), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+SELECT
+  'http://exampleidprovider.org', '1234567890', 'rmaptestuser', 'RMap Test User', 'https://exampleidprovider.org/rmaptestuser', (select userId from Users where name='RMap Test User'), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+WHERE NOT EXISTS (
+  SELECT * FROM UserIdentityProviders WHERE providerAccountId = '1234567890'
+);
 
 INSERT into ApiKeys(accessKey, secret, keyUri, label, note, keyStatus, includeInEvent, userId, createdDate, lastModifiedDate)
-values ('uah2CKDaBsEw3cEQ', 'NSbdzctrP46ZvhTi', 'rmap:testkeyuri', 'RMap test key', 'Test key for RMap', 'ACTIVE', FALSE, (select userId from Users where name='RMap Test User'), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP);
+SELECT
+  'uah2CKDaBsEw3cEQ', 'NSbdzctrP46ZvhTi', 'rmap:testkeyuri', 'RMap test key', 'Test key for RMap', 'ACTIVE', FALSE, (select userId from Users where name='RMap Test User'), CURRENT_TIMESTAMP, CURRENT_TIMESTAMP
+WHERE NOT EXISTS (
+  SELECT * FROM ApiKeys WHERE keyUri = 'rmap:testkeyuri'
+);
